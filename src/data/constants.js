dbcdbdc43a403890f40b50734738064c6b321851
Mansheen Kaur <manankaur2000@gmail.com>
2024-11-07 15:17:38 +1100

    portfolio

---
+++ src/data/constants.js
@@ -0,0 +1,272 @@
+export const Bio = {
+  name: "Mansheen Kaur",
+  roles: ["Frontend Developer", "Web Developer", "Programmer"],
+  description:
+    "A versatile developer with expertise in Java, C/C++, HTML, Css, JavaScript, ReactJS. Whether crafting front-end magic or building designs , I turn ideas into seamless digital experiences. Let’s collaborate, create, and elevate together!",
+  github: "https://github.com/mansheen28",
+  resume:
+    "https://drive.google.com/file/d/1p9AtCcnJ2IVocl2yJLd8wKAhtQWHl93K/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/mansheen-kaur-9506b31a7/",
+  insta: "https://www.instagram.com/mansheennnk/",
+};
+
+export const skills = [
+  {
+    title: "Frontend",
+    skills: [
+      {
+        name: "React JS",
+        image:
+          "data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHZpZXdCb3g9Ii0xMS41IC0xMC4yMzE3NCAyMyAyMC40NjM0OCI+CiAgPHRpdGxlPlJlYWN0IExvZ288L3RpdGxlPgogIDxjaXJjbGUgY3g9IjAiIGN5PSIwIiByPSIyLjA1IiBmaWxsPSIjNjFkYWZiIi8+CiAgPGcgc3Ryb2tlPSIjNjFkYWZiIiBzdHJva2Utd2lkdGg9IjEiIGZpbGw9Im5vbmUiPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIi8+CiAgICA8ZWxsaXBzZSByeD0iMTEiIHJ5PSI0LjIiIHRyYW5zZm9ybT0icm90YXRlKDYwKSIvPgogICAgPGVsbGlwc2Ugcng9IjExIiByeT0iNC4yIiB0cmFuc2Zvcm09InJvdGF0ZSgxMjApIi8+CiAgPC9nPgo8L3N2Zz4K",
+      },
+      {
+        name: "Redux",
+        image: "https://cdn.worldvectorlogo.com/logos/redux.svg",
+      },
+      {
+        name: "HTML",
+        image: "https://www.w3.org/html/logo/badge/html5-badge-h-solo.png",
+      },
+      {
+        name: "CSS",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d5/CSS3_logo_and_wordmark.svg/1452px-CSS3_logo_and_wordmark.svg.png",
+      },
+      {
+        name: "JavaScript",
+        image: "https://cdn.worldvectorlogo.com/logos/javascript-1.svg",
+      },
+      {
+        name: "JQuery",
+        image:
+          "https://e7.pngegg.com/pngimages/662/163/png-clipart-jquery-logo-web-development-jquery-ui-javascript-computer-icons-jqlogo-emblem-label-thumbnail.png",
+      },
+      {
+        name: "Bootstrap",
+        image:
+          "https://getbootstrap.com/docs/5.3/assets/brand/bootstrap-logo-shadow.png",
+      },
+      {
+        name: "Material UI",
+        image:
+          "https://static-00.iconduck.com/assets.00/material-ui-icon-2048x1626-on580ia9.png",
+      },
+    ],
+  },
+
+  {
+    title: "Other Technologies",
+    skills: [
+      {
+        name: "Java",
+        image:
+          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+      },
+      {
+        name: "XML",
+        image: "https://www.svgrepo.com/show/31053/xml.svg",
+      },
+      {
+        name: "Git",
+        image:
+          "https://camo.githubusercontent.com/fbfcb9e3dc648adc93bef37c718db16c52f617ad055a26de6dc3c21865c3321d/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f6769742d73636d2f6769742d73636d2d69636f6e2e737667",
+      },
+      {
+        name: "GitHub",
+        image:
+          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+      },
+      {
+        name: "Figma",
+        image:
+          "https://w7.pngwing.com/pngs/911/515/png-transparent-figma-logo-brand-logos-brands-in-colors-icon-thumbnail.png",
+      },
+      {
+        name: "VS Code",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+      },
+      {
+        name: "Postman",
+        image:
+          "https://camo.githubusercontent.com/93b32389bf746009ca2370de7fe06c3b5146f4c99d99df65994f9ced0ba41685/68747470733a2f2f7777772e766563746f726c6f676f2e7a6f6e652f6c6f676f732f676574706f73746d616e2f676574706f73746d616e2d69636f6e2e737667",
+      },
+      {
+        name: "Python",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png",
+      },
+      {
+        name: "C++",
+        image:
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/1/18/ISO_C%2B%2B_Logo.svg/1822px-ISO_C%2B%2B_Logo.svg.png",
+      },
+      {
+        name: "Azure",
+        image: "https://swimburger.net/media/ppnn3pcl/azure.png",
+      },
+    ],
+  },
+];
+
+export const softskills = [
+  {
+    skills: [
+      {
+        name: "Communication",
+        image: "https://cdn-icons-png.flaticon.com/512/3820/3820148.png",
+      },
+      {
+        name: "Problem-solving",
+        image: "https://cdn-icons-png.flaticon.com/512/7399/7399787.png",
+      },
+      {
+        name: "Teamwork",
+        image: "https://cdn-icons-png.flaticon.com/512/236/236810.png",
+      },
+      {
+        name: "Leadership",
+        image:
+          "https://starfinderfoundation.org/wp-content/uploads/2021/07/kisspng-computer-icons-scalable-vector-graphics-business-team-icon-png-business-businessman-leader-5ab04efc073c13.4492848315215039960296.png",
+      },
+      {
+        name: "Time management",
+        image: "https://cdn-icons-png.flaticon.com/512/1897/1897866.png",
+      },
+      {
+        name: "Critical thinking",
+        image: "https://cdn-icons-png.flaticon.com/512/7096/7096001.png",
+      },
+      {
+        name: " Emotional intelligence",
+        image: "https://cdn-icons-png.flaticon.com/512/6000/6000898.png",
+      },
+      {
+        name: "Creativity",
+        image: "https://cdn-icons-png.flaticon.com/512/5985/5985765.png",
+      },
+    ],
+  },
+];
+
+export const experiences = [
+  {
+    id: 0,
+    img: "https://media.licdn.com/dms/image/v2/D560BAQHY4q5-ajMQzw/company-logo_200_200/company-logo_200_200/0/1702907971308/pepcoding_logo?e=2147483647&v=beta&t=_8iT1bEh4gfoyxCOtcbF_vRB_gFJRi1jjaTAFt-Oq4Y",
+    role: "Product Engineering Intern",
+    company: "Pepcoding Pvt Ltd.",
+    date: "Sept 2021 - Feb 2022",
+    desc: "I created engaging content for Data Structures and Algorithms to enhance learning experiences for the company’s programming courses. Additionally, I contributed to the Nados platform by performing debugging, conducting code reviews, and developing features using HTML, CSS, JavaScript, and ReactJS. I focused on enhancing code quality through rigorous code reviews, ensuring optimal performance and adherence to company-level coding standards. Collaborating closely with the team, I also developed and maintained components, contributing to the continuous improvement of the company’s programming ecosystem.",
+    skills: [
+      "Data Structures and Algorithms",
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "ReactJS",
+      "Debugging",
+      "Code Reviews",
+      "Feature Development",
+      "Collaboration",
+    ],
+  },
+];
+
+export const education = [
+  {
+    id: 0,
+    img: "https://upload.wikimedia.org/wikipedia/en/thumb/7/74/Deakin_University_Logo_2017.svg/1200px-Deakin_University_Logo_2017.svg.png",
+    school: "Deakin University",
+    date: "March 2023 - November 2024",
+    grade: "77% (Distinction)",
+    desc: "Pursuing Master's in Information Technology with a focus on Software Development and Services, cultivating expertise in cutting-edge technologies.",
+    degree: "Master of Information Technology (Professional)",
+  },
+  {
+    id: 1,
+    img: "https://www.sarkarijobalert.co.in/wp-content/uploads/2021/02/GGSIPU.png",
+    school: "Guru Gobind Singh Indraprastha University",
+    date: "August 2018 - July 2022",
+    grade: "8.30 CGPA",
+    desc: "I earned a Bachelor's in IT from GGSIPU, Delhi, with an 8.30 CGPA. Studied Data Structures & Algorithms, OOPs, DBMS, OS, and Computer Networks. Active Hello World society member.",
+    degree: "Bachelor of Technology - BTech, Information Technology",
+  },
+];
+
+export const projects = [
+  {
+    id: 0,
+    title: "Captiplay",
+    date: "Mar 2024 - May 2024",
+    description:
+      "I built an interactive video captioning tool using ReactJS, leveraging modern programming practices to enable users to upload videos and add random captions with precise timestamp synchronization. I developed logic to prevent overlapping or duplicate captions during video playback, ensuring a smooth and uninterrupted user experience. Additionally, I integrated a dynamic feature to delete captions while maintaining seamless video functionality and ensuring consistency within the database.",
+    image: "https://i.ibb.co/SX1Wymk/Screenshot-2024-11-07-144746.png",
+    tags: ["ReactJS", "HTML", "CSS", "JavaScript", "Material UI"],
+    category: "web app",
+    github: "https://github.com/mansheen28/Captiplay.git",
+    webapp: "https://mansheen28.github.io/Captiplay/",
+  },
+  {
+    id: 1,
+    title: "ReWearIt - E-Commerce Website",
+    date: "Apr 2023 - June 2023",
+    description:
+      "I designed the entire application's prototype using Figma, creating a visually appealing and user-friendly interface. For handling HTTP requests, I used Axios, enabling seamless data retrieval and transmission between the client and server. The application’s functionality includes Create, Read, Update, and Delete (CRUD) operations for efficiently managing photowall content, structured within the MVC architectural pattern for enhanced code organization and maintenance. To ensure scalability and optimal performance, I implemented MongoDB for efficient data storage and retrieval. Additionally, I added features like a dynamic photowall, refreshed home/logo, an enabled navbar scroll, and page redirection, providing a smooth and intuitive navigation experience.",
+    image:
+      "https://i.ibb.co/p2Hnpc3/Screenshot-2024-11-07-144013.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "NodeJs",
+      "Figma",
+      "MongoDB",
+      "Bootstrap",
+    ],
+    category: "web app",
+    github: "https://github.com/kaseyuu/sit725-2023-t1-teamwork",
+    webapp: "https://github.com/kaseyuu/sit725-2023-t1-teamwork",
+  },
+  {
+    id: 2,
+    title: "Whatsapp Web Clone",
+    date: "Sept 2022 - Nov 2022",
+    description:
+      "This project is a WhatsApp Web clone designed for real-time messaging, featuring a dynamic photowall, CRUD capabilities, and intuitive navigation. It includes user-friendly interfaces, efficient data handling with SQL, and seamless client-server communication via Axios. The prototype was crafted in Figma, ensuring a visually appealing and engaging user experience.",
+    image:
+      "https://i.ibb.co/p1MQ1LM/Screenshot-2024-11-07-144337.png",
+    tags: [
+      "HTML",
+      "CSS",
+      "JavaScript",
+      "Jquery",
+      "RESTful API",
+      "SQL",
+      "Firebase",
+      "Figma",
+    ],
+    category: "web app",
+    github: "https://github.com/mansheen28/whatsapp-web-clone.git",
+    webapp: "https://github.com/mansheen28/whatsapp-web-clone.git",
+  },
+  {
+    id: 3,
+    title: "Notties - The Notes Application",
+    date: "Jan 2024 - Feb 2024",
+    description:
+      "I developed a note-taking application called *Notties* using ReactJS and Material UI, focusing on providing a seamless and intuitive user experience. The application allows users to add notes through a form, saving them in local storage to ensure persistence. Users can edit existing notes directly within the interface and delete notes as needed, facilitating effective note management. Additionally, I integrated a search filter functionality, enabling users to search notes by name, which enhances usability and accessibility. Local storage ensures that notes are retained even after page reloads, providing a consistent and user-friendly experience.",
+    image:
+      "https://i.ibb.co/XpzCmcS/Screenshot-2024-11-07-145916.png",
+    tags: ["ReactJS", "HTML", "CSS", "JavaScript", "Material UI"],
+    category: "web app",
+    github: "https://github.com/mansheen28/notes_app",
+    webapp: "https://mansheen28.github.io/notes_app/",
+  },
+];
+
+export const TimeLineData = [
+  { year: 2017, text: "Started my journey" },
+  { year: 2018, text: "Worked as a freelance developer" },
+  { year: 2019, text: "Founded JavaScript Mastery" },
+  { year: 2020, text: "Shared my projects with the world" },
+  { year: 2021, text: "Started my own platform" },
+];
